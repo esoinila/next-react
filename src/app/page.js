222535b6d8a7a200c227ce59fc136ccdab71158e
ableRex358 <erno.soinila@gmail.com>
2023-06-15 13:28:09 +0300

    more next testing

--- src/app/page.js
+++ src/app/page.js
@@ -40,10 +40,10 @@ export default function Home() {
         />
       </div>
       <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px]">
-        <p>Hosting is the thing we're interested in. So that is what we will test.</p>
-        <p class="font-sans ...">The quick brown fox ...</p>
-        <p class="font-serif ...">The quick brown fox ...</p>
-        <p class="font-mono ...">The quick brown fox ...</p>
+        
+        <p class="font-sans text-sm ...">The quick brown fox ...</p>
+        <p class="font-serif text-base ...">The quick brown fox ...</p>
+        <p class="font-mono text-2xl ...">The big brown fox ...</p>
       </div>
 
       <div className="mb-32 grid text-center lg:mb-0 lg:grid-cols-4 lg:text-left">
